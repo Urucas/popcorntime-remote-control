24fd3b6fc85e4a3ba0d724086a1f3428e83d69f9
Bruno Alassia <vruno.alassia@gmail.com>
2014-11-05 01:41:14 -0300

    add appium test

--- tests/android-native.js
+++ tests/android-native.js
@@ -6,28 +6,31 @@ var test = new (function(){
 	
 	this.port = 4723; 
 	this.browser;
-	this.env = "as";
+	this.argv = process.argv.slice(2);
 
 	this.checkAppiumIsRunning = function() {
-		console.log("Checking Appium is running");
-		var appium_url = "http://127.0.0.1:"+this.port+"/wd/hub/status";
-		var request = require("request");
-		request(appium_url, function (error, response, body) {
-			if(error || response.statusCode != 200) {
+		var appiumRunning = require('appium-running');
+		appiumRunning(this.port, function(success){
+			if(!success){
 				instance.error("Appium is not running");
 				return;
 			}
 			console.log("Appium is running, move on!");
-			instance.buildApp();
-		});
+			if(instance.argv.indexOf("--no-build") !== -1) {
+				instance.sendTest();
+			}else{
+				instance.buildApp();
+			}
+		})
 	};
 
 	this.buildApp = function() {
 		var sys = require('sys')
 		var exec = require('child_process').exec;
 		var child;
+		// script to build apk with android studio project
 		console.log("Building android app: ./gradlew app:assembleDebug");
-		child = exec("./gradlew app:assembleRelease", function (error, stdout, stderr) {
+		child = exec("./gradlew app:assembleDebug", function (error, stdout, stderr) {
 			if (error !== null) { 
 				instance.error('Building error ', error);
 				return;
@@ -36,18 +39,14 @@ var test = new (function(){
 		});
 	};
 	
-	this.buildEclipseApp = function() {
-		
-	}
-
 	this.error = function(msg, err) {
-		console.error('TEST FAILED! ' + (msg?msg:''), (msg?err:msg));
-		process.exit(1);
+		console.error('TEST FAILED! ' + (msg?msg:''), (err?err:msg));
+		if(this.browser) this.browser.quit();
   }
 
 	this.quit = function() {
 		console.log("TEST OK!");
-		process.exit(1);
+		if(this.browser) this.browser.quit();
 	}
 
 	this.sendTest = function() {
@@ -57,40 +56,72 @@ var test = new (function(){
 		basePath = process.cwd();
 	
 		// test instance
-		var app = wd.remote('localhost', this.port);
+		var app = this.browser = wd.remote('localhost', this.port);
 		app.init({
 			deviceName: 'Android',
 			platformName: 'Android',
 			platformVersion: '4.4',
 			local: true,
-			"app": basePath + '/build/app-release.apk',
+			"app": basePath + '/app/build/outputs/apk/app-debug-unaligned.apk',
 			"appPackage": "com.urucas.ptremotecontrol", 
 			"appActivity": "com.urucas.popcorntimerc.activities.SplashActivity",
 			"appWaitActivity": "com.urucas.popcorntimerc.activities.SplashActivity",
 			automationName:'Selendroid'
 		}, function(err, session, caps) {
 			
+			// default values
+			var conn = {
+				host: "192.168.0.100",
+				port: 8008,
+				user: "popcorn",
+				pass: "popcorn"
+			};
+			
+			/*
+			var req = require("request");
+			req(conn.host+":"+conn.port, function (error, response, body) {
+				
+			});
+			app.quit();
+			*/
 			// test implementation
-			try {
-				if (err) instance.error(err);
-				else app.setImplicitWaitTimeout(10000, function(err) {
-					if (err) instance.error('error setting timeout', err);
-						app.elementById('showMoviestt', function(err, el) {
-							if(err) instance.error("error finding showMovies", err);
-							else{
-								el.click(function(err){
-									if(err) instance.error("error clicking showMovies", err);
-									else { 
-										app.quit(); 
-										// instance.quit(); 
-									};
-								});
-							}
-						})
-				});
-			}catch(e) {
-				instance.error("exception ", e);
-			}
+			app.setImplicitWaitTimeout(10000, function(err) {
+				if(err) instance.error("Error setting implicit timeout", err);
+				else app.elementById('menuBtt', function(err, el) {
+				if(err) instance.error("Error finding menuBtt element", err);
+				else el.click(function(err){
+				if(err) instance.error("Error clicking menuBtt");
+				else app.elementById('ptHost', function(err, el){
+				if(err) instance.error("Error finding ptHost elemen", err);
+				else el.clear(function(err){
+				if(err) instance.error("Error clearing ptHost value", err);
+				else el.type(conn.host, function(err){
+				if(err) instance.error("Error typing ptHost value", err);
+				else app.elementById('ptPort', function(err, el){
+				if(err) instance.error("Error finding ptPort element", err);
+				else el.clear(function(err){
+				if(err) instance.error("Error clearing ptPort value", err);
+				else el.type(conn.port, function(err){
+				if(err) instance.error("Error typing ptPort value", err);
+				else app.elementById("ptUser", function(err, el){
+				if(err) instance.error("Error finding ptUser element", err);
+				else el.clear(function(err){
+				if(err) instance.error("Error clearing ptUser value", err);
+				else el.type(conn.user, function(err){
+				if(err) instance.error("Error typing ptUser value", err);
+				else app.elementById("ptPass", function(err, el){
+				if(err) instance.error("Error finding ptPass element", err);
+				else el.clear(function(err){
+				if(err) instance.error("Error clearing ptPass value", err);
+				else el.type(conn.pass, function(err){
+				if(err) instance.error("Error typing ptPass value", err);
+				else app.elementById("connectBtt", function(err, el){
+				if(err) instance.error("Error finding connectBtt element", err);
+				else el.click(function(err){
+				if(err) instance.error("Error clicking connectBtt", err);
+				else instance.quit();
+		
+			});});});});});});});});});});});});});});});})});
 		});
 	};
 
